d7bd0b30f9b849fe4568a3ec5d7b0023bb3951ba
Heitor Chang <hcproradio@gmail.com>
2014-03-25 11:55:31 -0300

    auto update

--- chegando/generatepages.py
+++ chegando/generatepages.py
@@ -10,10 +10,6 @@ import odf2array as odsreader
 # http://www.marco83.com/work/173/read-an-ods-file-with-python-and-odfpy/
 
 os.chdir("c:/Users/Heitor/Desktop/emacs-24.3/bin/")
-# chegando da fabrica
-chegandofile = odsreader.ODSReader("N:/CHEGANDO.ods")
-chegandotable = chegandofile.getSheet("Planilha1")
-chegandohtml = open("pontual.github.io/chegando/chegando.html", 'w')
 
 commonheader = """
 <html>
@@ -68,28 +64,6 @@ chegandoheader = commonheader + """
   <tbody>
 """
 
-chegandobody = ""
-
-for row in chegandotable[1:]:
-    chegandobody += """
-    <tr>
-    <td>%s</td>
-    <td>%s</td>
-    <td>%s</td>
-    <td>%s</td>
-    <td>%s</td>
-    </tr>""" % tuple(row[1:])
-    
-print("%s %s %s" % (chegandoheader, chegandobody, chegandofooter),
-      file=chegandohtml)
-
-chegandohtml.close()
-
-# aguardando desistencia
-aguardarfile = odsreader.ODSReader("N:/AGUARDAR DESISTENCIA E CONTAINER.ods")
-aguardartable = aguardarfile.getSheet("Aguardar")
-aguardarhtml = open("pontual.github.io/chegando/aguardar.html", 'w')
-
 aguardarheader = commonheader + """
 <table id="myTable" class="tablesorter">
   <thead>
@@ -103,8 +77,20 @@ aguardarheader = commonheader + """
   <tbody>
 """
 
+chegandobody = ""
 aguardarbody = ""
+
+
+
+
+# aguardando desistencia
+aguardarfile = odsreader.ODSReader("N:/AGUARDAR DESISTENCIA E CONTAINER.ods")
+aguardartable = aguardarfile.getSheet("Aguardar")
+aguardarhtml = open("pontual.github.io/chegando/aguardar.html", 'w')
+
 lastcodigo = ""
+
+reservadocontainer = {}
 
 for row in aguardartable[1:]:
     # extrapolate missing columns
@@ -130,6 +116,17 @@ for row in aguardartable[1:]:
     <td>%s</td>
     </tr>""" % (row[0], row[3], row[4], row[5])
 
+    # add to reservadocontainer for column in chegando
+    codigo = row[0]
+    place = row[3]
+    qty = int(row[4])
+    
+    if place != "Desistencia":
+        if codigo not in reservadocontainer:
+            reservadocontainer[codigo] = qty
+        else:
+            reservadocontainer[codigo] += qty
+
     if len(row) == 6:
         lastcodigo = row[0]
 
@@ -137,3 +134,34 @@ print("%s %s %s" % (aguardarheader, aguardarbody, aguardarfooter),
       file=aguardarhtml)
 
 aguardarhtml.close()
+
+
+# chegando da fabrica
+chegandofile = odsreader.ODSReader("N:/CHEGANDO.ods")
+chegandotable = chegandofile.getSheet("Planilha1")
+chegandohtml = open("pontual.github.io/chegando/chegando.html", 'w')
+
+for row in chegandotable[1:]:
+    codigo = row[1]
+    chegandoqty = int(row[3])
+    chegandoreservado = reservadocontainer[codigo]
+    if chegandoreservado > chegandoqty:
+        rowreservado = chegandoqty
+    else:
+        rowreservado = chegandoreservado
+    chegandoreservado -= rowreservado
+    
+    chegandobody += """
+    <tr>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    </tr>""" % tuple(row[1:4]) + (rowreservado, chegandoqty - rowreservado)
+    
+print("%s %s %s" % (chegandoheader, chegandobody, chegandofooter),
+      file=chegandohtml)
+
+chegandohtml.close()
+
