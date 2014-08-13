d32751d9fa08589a132a7809657c869ff75f8978
Heitor Chang <hcproradio@gmail.com>
2014-08-13 13:22:14 -0300

    auto update

---
+++ scripts/copy_codigo_to_dimen.py
@@ -0,0 +1,7 @@
+import os
+import glob
+
+FOTOS_COM_CODIGO_DIR = "c:/Users/Heitor/Desktop/Fotos com codigos local/"
+
+FOTOS_DIMEN_DIR = "c:/Users/Heitor/Desktop/Fotos com codigos local/fotos dimensoes/"
+
